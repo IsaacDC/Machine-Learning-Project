ab6445494245b64b4e735eee05de937bf012d68b
Sebastian <spava002@fiu.edu>
2024-03-13 19:33:52 -0400

    Added more descriptive comments

--- app.py
+++ app.py
@@ -4,33 +4,48 @@ from flask_socketio import SocketIO
 from current_time import get_current_time
 import random
 
+# Assigns Flask application name
 app = Flask(__name__)
+
+# Assigns secret key to allow secure communication between site to backend data
 app.config['SECRET_KEY'] = '9y2g9t1H8x3PT7Ej8UC92VsU'
+# Assigns name of SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chats.db'
 
+# Creates database object using SQLAlchemy
 db = SQLAlchemy(app)
+# Creates WebSocket using SocketIO
 socketio = SocketIO(app)
 
+
+# Database creation
+# Columns defined for data the db will hold
 class Chats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(250), nullable=False)
     user = db.Column(db.String(25), nullable=False)
     time_sent = db.Column(db.String(7), nullable=False)
 
+    # Required, but not necessary.
+    # Used for debugging via printing database attributes
     def __repr__(self):
         return ""
 
 with app.app_context():
     db.create_all()
 
+
+# Used for logging purposes
+# Prints a confirmation that WebSocket is functioning
 @socketio.on('connect')
 def check_connection():
     print("WebSocket connected successfully.")
 
-# Websocket for chat
+
+# Websocket functionality when it receives a message
 @socketio.on('message')
 def handle_message(message):
-    # Adds message to chat database
+    # Add message along with extra data to Chats database
     user = session.get('user')
     time_sent = get_current_time()
     new_message = Chats(message=message, user=user, time_sent=time_sent)
@@ -41,20 +56,30 @@ def handle_message(message):
     message_data = {"user": user, "message": message, "time_sent": time_sent}
     socketio.emit('message', message_data)
 
-# Main Page
+
+# Main Route ('/') also known as the Home Page
+# Has access to POST and GET data to and from the backend
 @app.route("/", methods=['POST', 'GET'])
 def home_page():
+    # Create a random username and place in session for easy access later on
     session['user'] = "User" + str(random.randint(100, 999))
     user = session.get('user')
+    # Retrieve the latest 25 chats from Chats database (By sorting through highest Id)
     latest_chats = db.session.query(Chats).order_by(Chats.id.desc()).limit(25).all()
     latest_chats.reverse()
+    # Load 'home.html' with access to user and latest_chats
     return render_template("home.html", user=user, latest_chats=latest_chats)
 
+
 # Chat History *TESTING PURPOSES*
+# Has access to GET data from the backend
 @app.route("/chat-history", methods=['GET'])
 def chat_history():
+    # Get all Chats database data into a single variable
     chat_history = Chats.query.all()
+    # Load 'chatHistory,html' with access to chat_history
     return render_template("chatHistory.html", chat_history=chat_history)
+
 
 # Runs a local server with WebSocket support
 if __name__ == '__main__':
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,26 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io.connect('http://' + document.domain + ':' + location.port);
 
-    // Adds a new list item containing the message to the 'message-window' list in home page
+    // Adds a new list item containing the message
+    // Receives message data from handle_message() in backend
     socket.on('message', (data) => {
         const messageWindow = document.getElementById('message-window');
         const li = document.createElement('li');
         li.innerHTML = '(' + data.time_sent + ') ' + '<strong>' + data.user + '</strong>' + ': ' + data.message;
         messageWindow.appendChild(li);
 
-        // Limits message history to 25 messages
+        // Limits message history to 25 messages by removing top <li> when a new message is added
         const messages = messageWindow.getElementsByTagName('li');
         if (messages.length > 25) {
             messageWindow.removeChild(messages[0])
         }
     });
 
-    // On submit, gets message data and sends it to the websocket to add to chat list
+    // On submit, gets message data and sends it to the Websocket
     document.getElementById('chat-form').addEventListener('submit', (e) => {
         e.preventDefault();
         const newMessage = document.getElementById('new-message');
         const message = newMessage.value.trim();
         if (message) {
+            // Message sent to handle_message() in backend
             socket.emit('message', message);
             newMessage.value = '';
         }
